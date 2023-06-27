36a4fcba1c55179123710bd95c09a52e5a787f2d
biancabih <bianca_silvapereira1@hotmail.com>
2023-06-27 00:10:23 -0300

    portfolio

---
+++ app.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("botão-um");
+button.addEventListener("click", function clicar() {
+    console.log("Oi! Aprendi no cinema nosso")
+});
+const nome = prompt("Qual é o seu nome?")
+console.log("nome")
